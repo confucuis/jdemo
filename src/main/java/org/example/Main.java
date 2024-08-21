3bea35f1342c8f3f20d623a2f3fa0a2093c7adf5
confucuis <confucuis@github.com>
2024-08-21 02:10:54 +0000

    接口和抽象类

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 import org.example.User;
 import org.example.Role;
 
+import org.example.Led;
+import org.example.Lsd;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -16,19 +19,55 @@ public class Main {
 //            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
 //            System.out.println("i = " + i);
 //        }
-        System.out.println("hello world");
+//        System.out.println("hello world");
+//
+//        User user1 = new User("张三");
+//        System.out.println(user1.getName());
+//        user1.setName("张二娃");
+//        System.out.println(user1.getName());
+//
+//        User user2 = new User("李四");
+//        System.out.println(user2.getName());
+//        user2.setName("李四娃");
+//        System.out.println(user2.getName());
+//
+//        Role.setName("放牛娃儿");
+//        System.out.println(Role.getName());
 
-        User user1 = new User("张三");
-        System.out.println(user1.getName());
-        user1.setName("张二娃");
-        System.out.println(user1.getName());
+        // interface demo
+        Led led = new Led();
+        System.out.println("Led status is: " + led.getStatus());
+        led.on();
+        System.out.println("Led status is: " + led.getStatus());
+        led.off();
+        System.out.println("Led status is: " + led.getStatus());
 
-        User user2 = new User("李四");
-        System.out.println(user2.getName());
-        user2.setName("李四娃");
-        System.out.println(user2.getName());
+        System.out.println("==========================");
 
-        Role.setName("放牛娃儿");
-        System.out.println(Role.getName());
+        Lsd lsd = new Lsd();
+        System.out.println("Lsd status is: " + lsd.getStatus());
+        lsd.on();
+        System.out.println("Lsd status is: " + lsd.getStatus());
+        lsd.off();
+        System.out.println("Lsd status is: " + lsd.getStatus());
+
+        System.out.println("==========================");
+
+        // abstract demo
+        PrimarySchool primarySchool = new PrimarySchool();
+        System.out.println("我是: " + primarySchool.getGrade());
+        primarySchool.getYears("小学生");
+
+        MiddleSchool middleSchool = new MiddleSchool();
+        System.out.println("我是: " + middleSchool.getGrade());
+        middleSchool.getYears("初中生");
+
+        HighSchool highSchool = new HighSchool();
+        System.out.println("我是: " + highSchool.getGrade());
+        highSchool.getYears("高中生");
+
+        University university = new University();
+        System.out.println("我是: " + university.getGrade());
+        university.getYears("大学生");
     }
 }
